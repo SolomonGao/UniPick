2073ba9fd04d0807df5ca96203af259c7783bec3
SolomonGao <xinggao@github.com>
2026-02-13 18:13:32 -0500

    1

--- apps/web/src/components/Marketplace.tsx
+++ apps/web/src/components/Marketplace.tsx
@@ -6,7 +6,7 @@ import SearchableFeed from './SearchableFeed';
 export default function Marketplace() {
   return (
     // 在 React 组件内部包裹，Context 关系绝对安全
-    <Providers>
+    <Providers >
       <div className="p-4">
          {/* 这里的 SearchableFeed 能够百分百读到 Providers 里的 QueryClient */}
          <SearchableFeed />
